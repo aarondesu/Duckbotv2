3bc8d46c2f334e576559a41687b6c9b0f0eaee54
shinudesu <arokidaaron93@gmail.com>
2022-07-28 18:33:31 +0800

    refactor: redid eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
-  rules: {
-    'no-consoles': 'off',
-    'import/no-cycle': 'off',
-    'no-restricted-syntax': 'off',
-    'import/no-dynamic-require': 'off',
-    'global-require ': 'off',
-    'new-cap': 'off',
-    'no-unused-vars': 'warn',
-  },
-};
+    env: {
+        "es2021": true,
+        "node": true
+    },
+    extends: [
+        "airbnb-base",
+        "airbnb-typescript/base",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+    },
+    plugins: [
+        "@typescript-eslint"
+    ],
+    rules: {
+      'no-consoles': 'off',
+      'import/no-cycle': 'off',
+      'no-restricted-syntax': 'off',
+      'import/no-dynamic-require': 'off',
+      'global-require ': 'off',
+      'new-cap': 'off',
+      'no-unused-vars': 'warn',
+    }
+}
